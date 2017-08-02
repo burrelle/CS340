59520cf2a17682e5764e3ae7fede0e7cfe32381d
burrelle <burrelle@oregonstate.edu>
2017-08-02 09:38:15 -0700

    Added positionGroup table

---
+++ Final Project/positionGroup.sql
@@ -0,0 +1,4 @@
+-- Create the postion group tables
+-- INSERT INTO `postionGroup`(`positionGroup`) VALUES ([value-1])
+INSERT INTO `postionGroup`(`positionGroup`) VALUES
+('Offense'),('Defense'),('Special Teams');
