7558894aba16b56f05624e3b46b25b2e7bbce71b
burrelle <burrelle@oregonstate.edu>
2017-08-02 09:58:10 -0700

    Fixed formatting

--- Final Project/position.sql
+++ Final Project/position.sql
@@ -2,11 +2,11 @@
 -- INSERT INTO `position` (`positionGroup`, `position`) VALUES (SELECT positionGroupID from positionGroup WHERE positionGroup = '(O/D/ST)')
 
 INSERT INTO `position` (`positionGroup`, `position`) VALUES
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense', 'QB'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense', 'RB'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense', 'WR'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'DB'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'DE'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'LB'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Special Teams', 'K'),
-(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'P'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense'), 'QB'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense'), 'RB'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense'), 'WR'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense'), 'DB'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense'), 'DE'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense'), 'LB'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Special Teams'), 'K'),
+((SELECT positionGroupID from positionGroup WHERE positionGroup = 'Special Teams'), 'P');
