fa62efedc78cf62796c6817b53ed81c29ea1429b
burrelle <burrelle@oregonstate.edu>
2017-08-02 09:49:19 -0700

    Added position table

---
+++ Final Project/position.sql
@@ -0,0 +1,12 @@
+-- Adding the positions with the foriegn key from positionGroup
+-- INSERT INTO `position` (`positionGroup`, `position`) VALUES (SELECT positionGroupID from positionGroup WHERE positionGroup = '(O/D/ST)')
+
+INSERT INTO `position` (`positionGroup`, `position`) VALUES
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense', 'QB'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense', 'RB'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Offense', 'WR'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'DB'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'DE'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'LB'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Special Teams', 'K'),
+(SELECT positionGroupID from positionGroup WHERE positionGroup = 'Defense', 'P'),
