4d4ebf9951f8096bec702ee7e4a802fa6df08704
burrelle <burrelle@oregonstate.edu>
2017-08-02 16:27:49 -0700

    Added WR

--- Final Project/player.sql
+++ Final Project/player.sql
@@ -15,6 +15,15 @@ INSERT INTO `player`(`pNumber`, `firstName`, `lastName`, `age`, `team`, `positio
 ('25', 'LeSean', 'McCoy', '29', (SELECT teamID FROM teams WHERE mascot = 'Bills'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'RB')),
 ('24', 'Jordan', 'Howard', '22', (SELECT teamID FROM teams WHERE mascot = 'Bears'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'RB')),
 ('24', 'Devonta', 'Freeman', '25', (SELECT teamID FROM teams WHERE mascot = 'Falcons'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'RB')),
-('28', 'Melvin', 'Gordon', '24', (SELECT teamID FROM teams WHERE mascot = 'Chargers'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'RB'));
+('28', 'Melvin', 'Gordon', '24', (SELECT teamID FROM teams WHERE mascot = 'Chargers'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'RB')),
+('84', 'Antonio', 'Brown', '29', (SELECT teamID FROM teams WHERE mascot = 'Steelers'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+('13', 'Odell', 'Beckham Jr', '24', (SELECT teamID FROM teams WHERE mascot = 'Giants'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+('11', 'Julio', 'Jones', '28', (SELECT teamID FROM teams WHERE mascot = 'Falcons'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+('87', 'Jordy', 'Nelson', '32', (SELECT teamID FROM teams WHERE mascot = 'Packers'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+('13', 'Mike', 'Evans', '23', (SELECT teamID FROM teams WHERE mascot = 'Buccaneers'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+('13', 'Michael', 'Thomas', '23', (SELECT teamID FROM teams WHERE mascot = 'Saints'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+('18', 'AJ', 'Green', '29', (SELECT teamID FROM teams WHERE mascot = 'Bengals'), (SELECT positionGroupID FROM positionGroup WHERE positionGroup = 'Offense'), (SELECT positionID FROM positions WHERE position = 'WR')),
+
+
 
 -- TODO Add WR, D, ST
