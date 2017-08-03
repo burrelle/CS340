337b8952512520355c072d5e22e41ed1a082b5c9
burrelle <burrelle@oregonstate.edu>
2017-08-03 11:06:11 -0700

    Added template for offsensiveStats table

---
+++ Final Project/offensiveStats.sql
@@ -0,0 +1,3 @@
+-- Add offensive stats for the players that are on offense.
+-- INSERT INTO `offsensiveStats`( `passingAttempts`, `passesCompleted`, `passingYards`, `rushingYards`, `rushingAttempts`, `receptions`, `targets`, `receivingYards`) VALUES
+INSERT INTO `offsensiveStats`( `passingAttempts`, `passesCompleted`, `passingYards`, `rushingYards`, `rushingAttempts`, `receptions`, `targets`, `receivingYards`) VALUES 
