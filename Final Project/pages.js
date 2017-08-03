6fcfe0d3b98c66c0c3ccb28fbfe31a634b37464d
burrelle <burrelle@oregonstate.edu>
2017-08-03 16:36:05 -0700

    Database exploration

--- Final Project/pages.js
+++ Final Project/pages.js
@@ -2,7 +2,9 @@ var express = require('express');
 var mysql = require('./dbcon.js');
 
 var app = express();
-var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+var handlebars = require('express-handlebars').create({
+  defaultLayout: 'main'
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -10,10 +12,10 @@ app.set('port', 3000);
 app.use(express.static('public'));
 
 //Teams Page to show all of the teams in the NFL
-app.get('/teams', function(req,res, next){
+app.get('/teams', function(req, res, next) {
   var context = {};
-  mysql.pool.query('SELECT * FROM teams', function(err, rows, fields){
-    if(err){
+  mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
+    if (err) {
       next(err);
       return;
     }
@@ -23,13 +25,13 @@ app.get('/teams', function(req,res, next){
 });
 
 //404 Page
-app.use(function(req,res){
+app.use(function(req, res) {
   res.status(404);
   res.render('404');
 });
 
 //500 Page
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.type('plain/text');
   res.status(500);
