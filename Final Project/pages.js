0aebbfaee771205ad3ceaabf46e7d20d9e24f73e
burrelle <burrelle@oregonstate.edu>
2017-08-06 16:40:38 -0700

    Started adding an insert page for all of the tables.

--- Final Project/pages.js
+++ Final Project/pages.js
@@ -11,17 +11,42 @@ app.set('view engine', 'handlebars');
 app.set('port', 3000);
 app.use(express.static('public'));
 
+
 //Teams Page to show all of the teams in the NFL
 app.get('/teams', function(req, res, next) {
-  var context = {};
-  mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
-    if (err) {
-      next(err);
-      return;
-    }
-    context.results = rows;
-    res.render('teams', context)
-  });
+var context = {};
+mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
+  if (err) {
+    next(err);
+    return;
+  }
+  context.results = rows;
+  res.render('teams', context)
+});
+});
+
+//TODO Make a page of insert into table
+app.use('/insert', function(req, res){
+  res.render('insert');
+});
+
+//Demo to try and insert a team.
+app.get('/teams-insert', function(req, res, next) {
+var context = {};
+mysql.pool.query('INSERT INTO `teams`(`city`, `mascot`, `stadium`) VALUES (?, ?, ?)',[req.query.city, req.query.mascot, req.query.stadium], function(err, result) {
+  if (err) {
+    next(err);
+    return;
+  }
+mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
+  if (err) {
+    next(err);
+    return;
+  }
+  context.results = rows;
+  res.render('teams', context)
+});
+});
 });
 
 //404 Page
