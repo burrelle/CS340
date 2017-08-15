9f036b3d43a8c4a8aa2763b759465fd0ea55495c
burrelle <burrelle@oregonstate.edu>
2017-08-15 14:03:04 -0700

    Formatting

--- Final Project/pages.js
+++ Final Project/pages.js
@@ -135,193 +135,199 @@ app.get('/teams-insert', getPositionGroups, getPositions, getPlayers, getOffensi
       next(err);
       return;
     }
-  mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
+    mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
       if (err) {
         next(err);
         return;
       }
-    req.teams = rows;
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
+      req.teams = rows;
+      res.render('insert', {
+        teams: req.teams,
+        positionGroup: req.positionGroup,
+        positions: req.positions,
+        players: req.players,
+        offsensiveStats: req.offsensiveStats,
+        defensiveStats: req.defensiveStats,
+        specialTeamsStats: req.specialTeamsStats
+      });
     });
   });
-});
 });
 
 
 app.get('/positionGroup-insert', function(req, res, next) {
-  mysql.pool.query('INSERT INTO `positionGroup`(`positionGroup`) VALUES (?)', [req.query.positionGroup], function(err, result) {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
-    });
-  });
-});
-
-
-//TODO Positions Insert specifically dealing with the foreign key
-app.get('/positions-insert', function(req, res, next) {
-  mysql.pool.query('INSERT INTO `positions` (`positionGroup`, `position`) VALUES  (?,?)', [req.query.positionGroup, req.query.position], function(err, result) {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
-    });
-  });
-});
-
-app.get('/player-insert', function(req, res, next) {
-  mysql.pool.query('INSERT INTO `player`(`pNumber`, `firstName`, `lastName`, `age`, `team`, `positionGroup`, `position`) VALUES (?,?,?,?,?,?,?)', [req.query.pNumber, req.query.firstName, req.query.lastName, req.query.age, req.query.mascot, req.query.positionGroup, req.query.position], function(err, result) {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
-    });
-  });
-});
-
-app.get('/offsensiveStats-insert', function(req, res, next) {
-  mysql.pool.query('INSERT INTO `offsensiveStats`(`playerNumber`, `passingAttempts`, `passesCompleted`, `passingYards`, `rushingYards`, `rushingAttempts`, `receptions`, `targets`,`receivingYards`)VALUES (?,?,?,?,?,?,?,?,?)', [req.query.playerNumber, req.query.passingAttempts, req.query.passesCompleted, req.query.passingYards, req.query.rushingYards, req.query.rushingAttempts, req.query.receptions, req.query.targets, req.query.receivingYards], function(err, result) {
-    if (err) {
-      next(err);
-      return;
-    }
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
-    });
-  });
-});
-
-app.get('/defensiveStats-insert', function(req,res,next){
-  mysql.pool.query('INSERT INTO `defensiveStats`(`playerNumber`, `sacks`, `tackles`,`forcedFumbles`,`interceptions`) VALUES (?,?,?,?,?)', [req.query.playerNumber, req.query.sacks, req.query.tackles, req.query.forcedFumbles, req.query.interceptions], function(err,result){
-    if (err) {
-      next(err);
-      return;
-    }
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
-    });
-  });
-});
-
-app.get('/specialTeamsStats-insert', function(req,res,next){
-  mysql.pool.query('INSERT INTO `specialTeamsStats`(`playerNumber`, `fieldGoalAttempts`, `fieldGoalMade`,`punts`,`avergePuntYards`) VALUES (?,?,?,?,?)', [req.query.playerNumber, req.query.fieldGoalAttempts, req.query.fieldGoalMade, req.query.punts, req.query.avergePuntYards], function(err,result){
-    if (err) {
-      next(err);
-      return;
-    }
-    res.render('insert', {
-      teams: req.teams,
-      positionGroup: req.positionGroup,
-      positions: req.positions,
-      players: req.players,
-      offsensiveStats: req.offsensiveStats,
-      defensiveStats: req.defensiveStats,
-      specialTeamsStats: req.specialTeamsStats
-    });
-  });
-});
-
-//Update Functions
-app.get('/teams-update', getTeams, getPositionGroups, getPositions, getPlayers, getOffensiveStats, getDefensiveStats, getSpecialTeamsStats, function(req,res,next){
-  var context = {};
-  mysql.pool.query("SELECT * FROM teams WHERE teamID=?", [req.query.teamID], function(err, result){
-    if (err) {
-      next(err);
-      return;
-    }
-    if(result.length == 1){
-      var curVals = result[0];
-      mysql.pool.query("UPDATE teams SET city=?, mascot=?, stadium=? WHERE teamID=?", [req.query.city||curVals.city, req.query.mascot||curVals.mascot, req.query.stadium||curVals.stadium, req.query.teamID], function(err,result){
+      mysql.pool.query('INSERT INTO `positionGroup`(`positionGroup`) VALUES (?)', [req.query.positionGroup], function(err, result) {
         if (err) {
+          next(err);
+          return;
+        }
+        mysql.pool.query('SELECT * FROM positionGroup', function(err, rows, fields) {
+          if (err) {
             next(err);
             return;
           }
-          mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
-            if (err) {
-              next(err);
-              return;
-            }
-            req.teams = rows;
-            res.render('tables', {
-              teams: req.teams,
-              positionGroup: req.positionGroup,
-              positions: req.positions,
-              players: req.players,
-              offsensiveStats: req.offsensiveStats,
-              defensiveStats: req.defensiveStats,
-              specialTeamsStats: req.specialTeamsStats
-            });
+          req.positionGroup = rows;
+          res.render('insert', {
+            teams: req.teams,
+            positionGroup: req.positionGroup,
+            positions: req.positions,
+            players: req.players,
+            offsensiveStats: req.offsensiveStats,
+            defensiveStats: req.defensiveStats,
+            specialTeamsStats: req.specialTeamsStats
           });
+        });
       });
-    }
-  });
-});
 
-//TODO UPDATE functions for all of the remaining tables
 
-//Homepage
-app.get('/', function(req, res) {
-  res.render('home');
-})
+      //TODO Positions Insert specifically dealing with the foreign key
+      app.get('/positions-insert', function(req, res, next) {
+        mysql.pool.query('INSERT INTO `positions` (`positionGroup`, `position`) VALUES  (?,?)', [req.query.positionGroup, req.query.position], function(err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('insert', {
+            teams: req.teams,
+            positionGroup: req.positionGroup,
+            positions: req.positions,
+            players: req.players,
+            offsensiveStats: req.offsensiveStats,
+            defensiveStats: req.defensiveStats,
+            specialTeamsStats: req.specialTeamsStats
+          });
+        });
+      });
 
-//404 Page
-app.use(function(req, res) {
-  res.status(404);
-  res.render('404');
-});
+      app.get('/player-insert', function(req, res, next) {
+        mysql.pool.query('INSERT INTO `player`(`pNumber`, `firstName`, `lastName`, `age`, `team`, `positionGroup`, `position`) VALUES (?,?,?,?,?,?,?)', [req.query.pNumber, req.query.firstName, req.query.lastName, req.query.age, req.query.mascot, req.query.positionGroup, req.query.position], function(err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('insert', {
+            teams: req.teams,
+            positionGroup: req.positionGroup,
+            positions: req.positions,
+            players: req.players,
+            offsensiveStats: req.offsensiveStats,
+            defensiveStats: req.defensiveStats,
+            specialTeamsStats: req.specialTeamsStats
+          });
+        });
+      });
 
-//500 Page
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.type('plain/text');
-  res.status(500);
-  res.render('500');
-});
+      app.get('/offsensiveStats-insert', function(req, res, next) {
+        mysql.pool.query('INSERT INTO `offsensiveStats`(`playerNumber`, `passingAttempts`, `passesCompleted`, `passingYards`, `rushingYards`, `rushingAttempts`, `receptions`, `targets`,`receivingYards`)VALUES (?,?,?,?,?,?,?,?,?)', [req.query.playerNumber, req.query.passingAttempts, req.query.passesCompleted, req.query.passingYards, req.query.rushingYards, req.query.rushingAttempts, req.query.receptions, req.query.targets, req.query.receivingYards], function(err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('insert', {
+            teams: req.teams,
+            positionGroup: req.positionGroup,
+            positions: req.positions,
+            players: req.players,
+            offsensiveStats: req.offsensiveStats,
+            defensiveStats: req.defensiveStats,
+            specialTeamsStats: req.specialTeamsStats
+          });
+        });
+      });
 
-app.listen(app.get('port'), function() {
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-});
+      app.get('/defensiveStats-insert', function(req, res, next) {
+        mysql.pool.query('INSERT INTO `defensiveStats`(`playerNumber`, `sacks`, `tackles`,`forcedFumbles`,`interceptions`) VALUES (?,?,?,?,?)', [req.query.playerNumber, req.query.sacks, req.query.tackles, req.query.forcedFumbles, req.query.interceptions], function(err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('insert', {
+            teams: req.teams,
+            positionGroup: req.positionGroup,
+            positions: req.positions,
+            players: req.players,
+            offsensiveStats: req.offsensiveStats,
+            defensiveStats: req.defensiveStats,
+            specialTeamsStats: req.specialTeamsStats
+          });
+        });
+      });
+
+      app.get('/specialTeamsStats-insert', function(req, res, next) {
+        mysql.pool.query('INSERT INTO `specialTeamsStats`(`playerNumber`, `fieldGoalAttempts`, `fieldGoalMade`,`punts`,`avergePuntYards`) VALUES (?,?,?,?,?)', [req.query.playerNumber, req.query.fieldGoalAttempts, req.query.fieldGoalMade, req.query.punts, req.query.avergePuntYards], function(err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          res.render('insert', {
+            teams: req.teams,
+            positionGroup: req.positionGroup,
+            positions: req.positions,
+            players: req.players,
+            offsensiveStats: req.offsensiveStats,
+            defensiveStats: req.defensiveStats,
+            specialTeamsStats: req.specialTeamsStats
+          });
+        });
+      });
+
+      //Update Functions
+      app.get('/teams-update', getTeams, getPositionGroups, getPositions, getPlayers, getOffensiveStats, getDefensiveStats, getSpecialTeamsStats, function(req, res, next) {
+        var context = {};
+        mysql.pool.query("SELECT * FROM teams WHERE teamID=?", [req.query.teamID], function(err, result) {
+          if (err) {
+            next(err);
+            return;
+          }
+          if (result.length == 1) {
+            var curVals = result[0];
+            mysql.pool.query("UPDATE teams SET city=?, mascot=?, stadium=? WHERE teamID=?", [req.query.city || curVals.city, req.query.mascot || curVals.mascot, req.query.stadium || curVals.stadium, req.query.teamID], function(err, result) {
+              if (err) {
+                next(err);
+                return;
+              }
+              mysql.pool.query('SELECT * FROM teams', function(err, rows, fields) {
+                if (err) {
+                  next(err);
+                  return;
+                }
+                req.teams = rows;
+                res.render('tables', {
+                  teams: req.teams,
+                  positionGroup: req.positionGroup,
+                  positions: req.positions,
+                  players: req.players,
+                  offsensiveStats: req.offsensiveStats,
+                  defensiveStats: req.defensiveStats,
+                  specialTeamsStats: req.specialTeamsStats
+                });
+              });
+            });
+          }
+        });
+      });
+
+      //TODO UPDATE functions for all of the remaining tables
+
+      //Homepage
+      app.get('/', function(req, res) {
+        res.render('home');
+      })
+
+      //404 Page
+      app.use(function(req, res) {
+        res.status(404);
+        res.render('404');
+      });
+
+      //500 Page
+      app.use(function(err, req, res, next) {
+        console.error(err.stack);
+        res.type('plain/text');
+        res.status(500);
+        res.render('500');
+      });
+
+      app.listen(app.get('port'), function() {
+        console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+      });
